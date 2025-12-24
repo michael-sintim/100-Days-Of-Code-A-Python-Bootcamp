e7fc83a44bd0881fee0326a41b9afaad20c5472a
Michael Sintim <michaelsintim673@gmail.com>
2025-12-24 07:22:31 +0000

    Day 1

--- Day_1.py
+++ Day_1.py
@@ -1,1 +1,12 @@
-print('heyee')
+# Band Name generator 
+# 
+print("she said \"Hi\" ")
+print("Hello world\n"*3)
+
+
+city = input("What city did you grow up in? ")
+pet = input("What name was your pet as a kid? ")
+
+band_name = f"Your Band name is:  {city} {pet}"
+
+print(band_name)
