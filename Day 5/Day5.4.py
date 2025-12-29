581c8bfde5da6462964a9b9f25a0a3976d1f464d
Michael Sintim <michaelsintim673@gmail.com>
2025-12-29 10:29:10 +0000

    Done for day 5

---
+++ Day 5/Day5.4.py
@@ -0,0 +1,6 @@
+
+total = 0 
+for x in range(2,101,2):
+    total +=  x                                                    
+
+print(total)
---
+++ Day 5/Day5.5.py
@@ -0,0 +1,16 @@
+# fizz Buzz program  
+# y= 0
+for x in range(1,101): 
+
+    
+    if x%5 == 0 and x%3==0:
+        print("Fizz Buzz")
+    elif x%3 == 0:
+        print("Fizz")
+
+    elif x%5 == 0:
+        print("Buzz")
+
+    else:
+        print(x)
+
